c0ce7647e72bbbed75a8f3052fa520fe7a6e8375
EliezerP <eliezerpolidor69@gmail.com>
2022-06-09 00:59:04 -0400

    se carga el segundo problema del reto en el cual se trabaja usando metodos de string

---
+++ ChallengeTwo.js
@@ -0,0 +1,48 @@
+const carta = 'bici coche balón _playstation bici coche peluche bici'
+
+const regalos = listGifts(carta)
+
+console.log(cartaRegalos)
+    // console.log(regalos)
+    /*
+    {
+      bici: 2,
+      coche: 2,
+      balón: 1,
+      peluche: 1
+    }
+    */
+
+
+function listGifts(carta) {
+    // ¡Tú puedes!
+    //leer la carta y crear un arreglo con cada regalo pedido en la carta
+    //contar en el arreglo la cantidad de veces que se repite un elemento
+    //pasar los elementos y la cantidad de veces al objeto 
+    letterGift = carta.split(" ").sort()
+    let uniqueGifts = []
+    let repeatedGifts = []
+    let contador = 1
+        // leterGift = {}
+    var cartaRegalos = new Object()
+
+    for (let i = 0; i < letterGift.length; i++) {
+        if (letterGift[i + 1] === letterGift[i]) {
+            contador++
+        } else {
+            uniqueGifts.push(letterGift[i])
+            repeatedGifts.push(contador)
+            contador = 1
+        }
+
+    }
+
+    for (let i = 0; i < uniqueGifts.length; i++) {
+        console.log('el regalo ' + uniqueGifts[i] + ' se repite ' + repeatedGifts[i])
+        console.log()
+
+        cartaRegalos.uniqueGifts[i] = repeatedGifts[i]
+    }
+
+    return cartaRegalos
+}
