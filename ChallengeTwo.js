4c596acb8542517a5dd4d7dc3c5c155f7c46c309
EliezerP <eliezerpolidor69@gmail.com>
2022-06-09 10:59:54 -0400

    se cambio el array letterGifts por letterRegalos para despues eliminar los elementos que iniciaron con "_" y dejar en el array letterGifts solo los elementos que no contengan este caracter

--- ChallengeTwo.js
+++ ChallengeTwo.js
@@ -2,16 +2,16 @@ const carta = 'bici coche balón _playstation bici coche peluche bici'
 
 const regalos = listGifts(carta)
 
-console.log(cartaRegalos)
-    // console.log(regalos)
-    /*
-    {
-      bici: 2,
-      coche: 2,
-      balón: 1,
-      peluche: 1
-    }
-    */
+// console.log(cartaRegalos)
+// console.log(regalos)
+/*
+{
+  bici: 2,
+  coche: 2,
+  balón: 1,
+  peluche: 1
+}
+*/
 
 
 function listGifts(carta) {
@@ -19,13 +19,24 @@ function listGifts(carta) {
     //leer la carta y crear un arreglo con cada regalo pedido en la carta
     //contar en el arreglo la cantidad de veces que se repite un elemento
     //pasar los elementos y la cantidad de veces al objeto 
-    letterGift = carta.split(" ").sort()
+    letterRegalos = carta.split(" ").sort()
     let uniqueGifts = []
     let repeatedGifts = []
     let contador = 1
         // leterGift = {}
     var cartaRegalos = new Object()
+    letterGift = []
 
+    for (let i = 0; i < letterRegalos.length; i++) {
+        const x = letterRegalos[i]
+        if (x.substr(0, 1) == "_") {
+            console.log(x.substr(0, 1))
+        } else {
+            letterGift.push(letterRegalos[i])
+        }
+
+    }
+    console.log(letterGift)
     for (let i = 0; i < letterGift.length; i++) {
         if (letterGift[i + 1] === letterGift[i]) {
             contador++
@@ -39,9 +50,9 @@ function listGifts(carta) {
 
     for (let i = 0; i < uniqueGifts.length; i++) {
         console.log('el regalo ' + uniqueGifts[i] + ' se repite ' + repeatedGifts[i])
-        console.log()
+            // console.log()
 
-        cartaRegalos.uniqueGifts[i] = repeatedGifts[i]
+        // cartaRegalos.uniqueGifts[i] = repeatedGifts[i]
     }
 
     return cartaRegalos
